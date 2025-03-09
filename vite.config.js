3ee6a3540a3fe11f66ce65bb38c7014359dfeeeb
Sunny <kajimalu10@gmail.com>
2025-03-10 03:10:16 +0900

    fit PWA

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,50 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'しまね交通機関ナビ', // アプリケーションリスト等で表示される名前
+        short_name: 'しまナビ', // ホーム画面で表示される名前
+        description: 'しまね交通機関ナビ',
+        theme_color: '#000000', // テーマカラー　最低限指定が必要
+        display: 'standalone', // 表示モード standaloneにするとネイティブアプリっぽくなる
+        icons: [ // 先ほどコンソールに出力されたものをコピペでOK
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
